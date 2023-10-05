8fcc27f3ad4b65d8f580175a68cf0d3d6f5975d6
uzairjacobs <93554483+uzairjacobs@users.noreply.github.com>
2023-10-05 10:25:33 +0200

    fixed code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 form.addEventListener("submit", (event) => {
@@ -8,10 +9,10 @@ form.addEventListener("submit", (event) => {
     if (dividend === '' || divider === '') {
       result.innerHTML = 'Division not performed. Both values are required in inputs. Try again.';
 
-    } else if (divider < 0) {
+    } else if (divider <= 0) {
       result.innerHTML = 'Division not performed. Invalid number provided. Try again.';
       throw new RangeError('Divider is not within the valid range');
-      
+
     } else if (isNaN(dividend) || isNaN(divider)) {
       document.body.innerHTML = 'Something critical went wrong. Please reload the page.';
       throw new TypeError('Dividend or Divider is not a number.');
@@ -20,8 +21,8 @@ form.addEventListener("submit", (event) => {
       result.innerText = Math.floor(dividend / divider);
     }
 
-  } catch (error) {
+  } catch (e) {
 
-    console.error(error);
+    console.log(e);
   }
 });
